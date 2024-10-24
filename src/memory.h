7348fdcd34976a0c42be1f28cf67e05ee010ad46
caiolaytynher <csrlaytynher@proton.me>
2024-10-23 22:19:42 -0300

    redesigned the api for memory

--- src/memory.h
+++ src/memory.h
@@ -4,40 +4,49 @@
 
 #define DEFAULT_ALIGNMENT (2 * sizeof(void*))
 
-typedef enum Allocation_Type {
-    Allocation_Type_Alloc,
-    Allocation_Type_Dealloc,
-    Allocation_Type_Realloc,
-    Allocation_Type_Resize,
-    Allocation_Type_Reset
-} Allocation_Type;
-
-typedef struct Allocation_Result {
-    void* memory;
-    Error error;
-} Allocation_Result;
-
-typedef Allocation_Result (*Allocator_Proc)(Allocation_Type type, void* memory, usize old_size, usize new_size);
-
-typedef struct Allocator {
-    void* data;
-    Allocator_Proc proc;
-} Allocator;
-
-extern Allocation_Result mem_alloc(usize size, Allocator allocator);
-extern Allocation_Result mem_realloc(void* memory, usize size, Allocator allocator);
-extern Allocation_Result mem_resize(void* memory, usize old_size, usize new_size, Allocator allocator);
-extern Error mem_dealloc(void* memory, Allocator allocator);
-extern Error mem_reset(Allocator allocator);
-
-// This is ment to live to the hole life of the program, no need to free it.
-typedef struct Arena {
+// NOTE(caio): I'm guessing that this could be used throughout all allocators.
+// If I'm wrong, it's easy to change later.
+typedef struct Memory_Buffer_List {
     byte* buffer;
     usize length;
+    Memory_Buffer_List* next;
+} Memory_Buffer_List;
+
+typedef struct Arena {
+    Memory_Buffer_List buffer_list;
     usize current_offset;
     usize previous_offset;
     usize alignment;
-    Arena* next;
 } Arena;
+
+typedef enum Allocator_Mode {
+    Allocator_Mode_Alloc,
+    Allocator_Mode_Dealloc,
+    Allocator_Mode_Realloc,
+    Allocator_Mode_Resize,
+    Allocator_Mode_Reset
+} Allocator_Mode;
+
+typedef struct Allocator_Result {
+    void *memory;
+    Error error;
+} Allocator_Result;
+
+typedef Allocator_Result (*Allocator_Proc)(Allocator_Mode type, void *memory, usize old_size, usize new_size);
+
+typedef union Allocator_Data {
+    Arena *arena;
+} Allocator_Data;
+
+typedef struct Allocator {
+    Allocator_Data data;
+    Allocator_Proc proc;
+} Allocator;
+
+extern Allocator_Result allocator_alloc(Allocator allocator, usize size);
+extern Allocator_Result allocator_realloc(Allocator allocator, void *memory, usize size);
+extern Allocator_Result allocator_resize(Allocator allocator, void *memory, usize old_size, usize new_size);
+extern Error allocator_dealloc(Allocator allocator, void *memory);
+extern Error allocator_reset(Allocator allocator);
 
 extern Allocator arena_allocator_make();
