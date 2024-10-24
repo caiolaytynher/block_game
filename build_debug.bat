1a7d513c856b718e73581f86f58b8b4702190199
caiolaytynher <csrlaytynher@proton.me>
2024-10-23 22:21:37 -0300

    matched the include in the build script

--- build_debug.bat
+++ build_debug.bat
@@ -4,13 +4,14 @@ if not exist build mkdir build
 if not exist build\debug mkdir build\debug
 
 cl /Zi ^
+    /Ivendor\src ^
     /Ivendor\glfw\include ^
     /Ivendor\glad\include ^
     /Ivendor\cglm\include ^
     /Febuild\debug\block_game.exe ^
     /Fdbuild\debug\ ^
     /Fobuild\debug\ ^
-    src\main.c ^
+    src\*.c ^
     vendor\glad\src\glad.c ^
     User32.lib Gdi32.lib Shell32.lib opengl32.lib ^
     vendor\glfw\lib\debug\glfw3.lib ^
