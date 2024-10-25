ff3ef0c97e25cc7bcd64c96f1ddcd6e87196d3a8
caiolaytynher <csrlaytynher@proton.me>
2024-10-24 22:51:30 -0300

    added panic and runtime assert

--- src/core.h
+++ src/core.h
@@ -3,11 +3,25 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <assert.h> // TODO(caio): make my own version
 
 #define KiB(n) ((n) * 1024ull)
 #define MiB(n) (KiB(n) * 1024ull)
 #define GiB(n) (MiB(n) * 1024ull)
+
+// TODO(caio): Replace print with log when the logging system is ready
+#define panic(...)                                  \
+    printf("Panic at %s:%d\n", __FILE__, __LINE__); \
+    printf(##__VA_ARGS__);                          \
+    exit(1)
+#define runtime_assert(expression, ...)                            \
+    if (!(expression)) {                                           \
+        printf("Assertion failed at %s:%d\n", __FILE__, __LINE__); \
+        printf(#expression "\n");                                  \
+        printf(##__VA_ARGS__);                                     \
+        exit(1);                                                   \
+    }
 
 typedef int8_t i8;
 typedef int16_t i16;
