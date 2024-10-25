bed5890a634436271053cede95cdfe709dcab63d
caiolaytynher <csrlaytynher@proton.me>
2024-10-24 22:55:30 -0300

    removed realloc from the api in favor of resize

--- src/memory.h
+++ src/memory.h
@@ -2,41 +2,41 @@
 
 #include "core.h"
 
-#define DEFAULT_ALIGNMENT (2 * sizeof(void*))
-
-// NOTE(caio): I'm guessing that this could be used throughout all allocators.
-// If I'm wrong, it's easy to change later.
-typedef struct Memory_Buffer_List {
-    byte* buffer;
-    usize length;
-    Memory_Buffer_List* next;
-} Memory_Buffer_List;
+#define ALLOCATOR_DEFAULT_ALIGNMENT (2 * sizeof(void*))
 
 typedef struct Arena {
-    Memory_Buffer_List buffer_list;
+    byte* buffer;
+    usize length;
     usize current_offset;
     usize previous_offset;
     usize alignment;
+    struct Arena* next;
 } Arena;
+
+// The size of the arena is added back in the creation procedure
+#define ARENA_DEFAULT_SIZE (KiB(4) - sizeof(Arena))
 
 typedef enum Allocator_Mode {
     Allocator_Mode_Alloc,
     Allocator_Mode_Dealloc,
-    Allocator_Mode_Realloc,
     Allocator_Mode_Resize,
     Allocator_Mode_Reset
 } Allocator_Mode;
 
 typedef struct Allocator_Result {
-    void *memory;
+    void* memory;
     Error error;
 } Allocator_Result;
 
-typedef Allocator_Result (*Allocator_Proc)(Allocator_Mode type, void *memory, usize old_size, usize new_size);
-
 typedef union Allocator_Data {
-    Arena *arena;
+    Arena* arena;
 } Allocator_Data;
+
+typedef Allocator_Result (*Allocator_Proc)(Allocator_Data data,
+                                           Allocator_Mode mode,
+                                           void* memory,
+                                           usize old_size,
+                                           usize new_size);
 
 typedef struct Allocator {
     Allocator_Data data;
@@ -44,9 +44,9 @@ typedef struct Allocator {
 } Allocator;
 
 extern Allocator_Result allocator_alloc(Allocator allocator, usize size);
-extern Allocator_Result allocator_realloc(Allocator allocator, void *memory, usize size);
 extern Allocator_Result allocator_resize(Allocator allocator, void *memory, usize old_size, usize new_size);
 extern Error allocator_dealloc(Allocator allocator, void *memory);
 extern Error allocator_reset(Allocator allocator);
 
-extern Allocator arena_allocator_make();
+extern Allocator arena_allocator_create(usize size, usize alingment);
+extern Allocator arena_allocator_create_default();
--- src/memory_allocator.c
+++ src/memory_allocator.c
@@ -1,21 +1,17 @@
 #include "memory.h"
 
 Allocator_Result allocator_alloc(Allocator allocator, usize size) {
-    return allocator.proc(Allocator_Mode_Alloc, NULL, 0, size);
-}
-
-Allocator_Result allocator_realloc(Allocator allocator, void* memory, usize size) {
-    return allocator.proc(Allocator_Mode_Realloc, memory, 0, size);
+    return allocator.proc(allocator.data, Allocator_Mode_Alloc, NULL, 0, size);
 }
 
 Allocator_Result allocator_resize(Allocator allocator, void* memory, usize old_size, usize new_size) {
-    return allocator.proc(Allocator_Mode_Resize, memory, old_size, new_size);
+    return allocator.proc(allocator.data, Allocator_Mode_Resize, memory, old_size, new_size);
 }
 
 Error allocator_dealloc(Allocator allocator, void* memory) {
-    return allocator.proc(Allocator_Mode_Dealloc, memory, 0, 0).error;
+    return allocator.proc(allocator.data, Allocator_Mode_Dealloc, memory, 0, 0).error;
 }
 
 Error allocator_reset(Allocator allocator) {
-    return allocator.proc(Allocator_Mode_Reset, NULL, 0, 0).error;
+    return allocator.proc(allocator.data, Allocator_Mode_Reset, NULL, 0, 0).error;
 }
